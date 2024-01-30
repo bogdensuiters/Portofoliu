c9530928ad8badd651d075ea3383e0e5c3649218
Dragos Anghelahar <135159961+bogdensuiters@users.noreply.github.com>
2024-01-30 21:11:21 +0200

    media aritmetica a 10 numere

---
+++ media aritmetica a 10 nr.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, d, e, f, g, h, i, j, ma;
+	cout << "a= ";
+	cin >> a;
+	cout << "b= ";
+	cin >> b;
+	cout << "c= ";
+	cin >> c;
+	cout << "d= ";
+	cin >> d;
+	cout << "e= ";
+	cin >> e;
+	cout << "f= ";
+	cin >> f;
+	cout << "g= ";
+	cin >> g;
+	cout << "h= ";
+	cin >> h;
+	cout << "i= ";
+	cin >> i;
+	cout << "j= ";
+	cin >> j;
+
+	ma = (a + b + c + d + e + f + g + h + i + j) / 10;
+	cout << "(a+b+c+d+e+f+g+h+i+j)/10 = " << ma << "\n";
+
+
+		return 0;
+
+}
